095684c817a8b2a445eb1efca86fb2c32fa2716a
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2024-05-14 12:48:11 +0700

    Add API project

---
+++ PetCareSystem/PetCareSystem/Program.cs
@@ -0,0 +1,37 @@
+
+namespace PetCareSystem
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+
+			builder.Services.AddControllers();
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			app.UseSwagger();
+
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseAuthorization();
+
+
+			app.MapControllers();
+
+			app.Run();
+		}
+	}
+}
