61669d30b73879d16868df65898958f94efca774
Nguyen Huu Minh <nghuuminh03@gmail.com>
2024-05-18 16:30:58 +0700

    add material-tailwind package

--- PCS_front_end/tailwind.config.js
+++ PCS_front_end/tailwind.config.js
@@ -1,12 +1,15 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = withMT( {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+});
 
