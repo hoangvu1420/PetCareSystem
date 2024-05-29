5ecfa02d1e06f36715695c3a1ff62adbf9ae57d4
Trung01122003 <trunghoanggv@gmail.com>
2024-05-29 11:18:13 +0700

    homepage

---
+++ PCS_front_end/src/Pages/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Hero from "../Components/Hero";
+import Info from "../Components/Info";
+import BookAppointment from "../Components/BookAppointment";
+import Reviews from "../Components/Reviews";
+import Doctors from "../Components/Doctors";
+import Footer from "../Components/Footer";
+
+function Home() {
+  return (
+    <div className="home-section">
+      <Navbar />
+      <Hero />
+      <Info />
+      <BookAppointment />
+      <Reviews />
+      <Doctors />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
