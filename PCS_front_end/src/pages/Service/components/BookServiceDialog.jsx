69f61c43444fc6c51cb8cde5f6857f9b571123fe
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2024-06-05 18:20:03 +0700

    Add select pet in BookServiceDialog

--- PCS_front_end/src/pages/Service/components/BookServiceDialog.jsx
+++ PCS_front_end/src/pages/Service/components/BookServiceDialog.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import {
     Button,
     Dialog,
@@ -7,6 +7,8 @@ import {
     CardFooter,
     Typography,
     Input,
+    Select,
+    Option
 } from "@material-tailwind/react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 import axios from "axios";
@@ -15,18 +17,23 @@ import { toast } from "react-toastify";
 
 export default function BookServiceDialog(props) {
     const [open, setOpen] = useState(false);
-
+    const [pets, setPets] = useState([]);
     const [booking_data, setBookingData] = useState({
-        "petId": 13,
+        "petId": "",
         "groomingServiceId": props.id,
-        "bookingDate": null,
-        "notes": null
+        "bookingDate": "",
+        "notes": ""
     });
 
     const api_url = 'https://petcaresystem20240514113535.azurewebsites.net';
-    const { user_data, setUserData } = useContext(UserContext);
+    const { user_data } = useContext(UserContext);
 
-    const handleOpen = () => setOpen((cur) => !cur);
+    const handleOpen = () => {
+        setOpen((cur) => !cur);
+        if (!open) {
+            getPetByCurrentId();
+        }
+    };
 
     const handleChange = (e) => {
         let value = e.target.value;
@@ -40,8 +47,8 @@ export default function BookServiceDialog(props) {
         setBookingData({
             ...booking_data,
             [e.target.name]: value
-        })
-    }
+        });
+    };
 
     const onPlaced = () => {
         handleOpen();
@@ -55,16 +62,35 @@ export default function BookServiceDialog(props) {
         date.setHours(12, 0, 0, 0);
         bookingDataCopy.bookingDate = date.toISOString();
 
-        axios.post(api_url + '/api/grooming-service-bookings', booking_data)
+        axios.post(api_url + '/api/grooming-service-bookings', bookingDataCopy)
             .then((res) => {
                 console.log(res);
                 if (res.data.isSucceed === true) {
                     props.getGroomingServices();
-                    toast.success("Sửa thành công", { autoClose: 2000 });
+                    toast.success("Đặt thành công", { autoClose: 2000 });
                 }
             })
             .catch((e) => console.log(e));
-    }
+    };
+
+    const getPetByCurrentId = async () => {
+        try {
+            const response = await axios.get(`${api_url}/api/pets?userId=${JSON.parse(user_data).userInfo.id}`, {
+                headers: {
+                    Authorization: `Bearer ${JSON.parse(user_data).token}`
+                }
+            });
+            console.log(response);
+            if (response.data.isSucceed) {
+                setPets(response.data.data);
+            }
+        } catch (error) {
+            console.log(error);
+            if (error.response.status !== 404)
+                toast.error(error.response.data.errorMessages[0]);
+            setPets([]);
+        }
+    };
 
     return (
         <>
@@ -79,8 +105,20 @@ export default function BookServiceDialog(props) {
                         <Typography variant="h4" color="blue-gray">
                             Đặt dịch vụ
                         </Typography>
+                        <Select
+                            label="Chọn pet"
+                            name="petId"
+                            value={booking_data.petId}
+                            onChange={(e) => setBookingData({ ...booking_data, petId: e })}
+                        >
+                            {pets.map((pet) => (
+                                <Option key={pet.id} value={pet.id.toString()}>
+                                    {pet.name} - {pet.breed}
+                                </Option>
+                            ))}
+                        </Select>
                         <Input required name="bookingDate" value={booking_data.bookingDate} onChange={handleChange} label="Ngày" type="date" size="md" />
-                        <Input className="w-1/2" name="price" value={booking_data.notes} onChange={handleChange} label="Ghi chú" type="text" size="md" placeholder="Thêm ghi chú hoặc để trống" />
+                        <Input className="w-1/2" name="notes" value={booking_data.notes} onChange={handleChange} label="Ghi chú" type="text" size="md" placeholder="Thêm ghi chú hoặc để trống" />
                     </CardBody>
                     <CardFooter className="pt-0">
                         <Button variant="text" color="gray" onClick={handleOpen}>
