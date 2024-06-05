f969e7b2c33ad641984dfc5800271135eae62795
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2024-06-05 18:00:52 +0700

    Fix bug in BookServiceDialog

--- PCS_front_end/src/pages/Service/components/BookServiceDialog.jsx
+++ PCS_front_end/src/pages/Service/components/BookServiceDialog.jsx
@@ -29,9 +29,17 @@ export default function BookServiceDialog(props) {
     const handleOpen = () => setOpen((cur) => !cur);
 
     const handleChange = (e) => {
+        let value = e.target.value;
+
+        if (e.target.name === 'bookingDate') {
+            let date = new Date(value);
+            date.setHours(12, 0, 0, 0);
+            value = date.toISOString().split('T')[0]; // Only keep the date part
+        }
+
         setBookingData({
             ...booking_data,
-            [e.target.name]: e.target.value
+            [e.target.name]: value
         })
     }
 
@@ -39,7 +47,15 @@ export default function BookServiceDialog(props) {
         handleOpen();
 
         axios.defaults.headers.common['Authorization'] = "Bearer " + JSON.parse(user_data).token;
-        axios.put(api_url + '/api/grooming-service-bookings', booking_data)
+        console.log(booking_data);
+
+        // Convert bookingDate back to an ISO string with time set to 12:00 PM
+        let bookingDataCopy = { ...booking_data };
+        let date = new Date(bookingDataCopy.bookingDate);
+        date.setHours(12, 0, 0, 0);
+        bookingDataCopy.bookingDate = date.toISOString();
+
+        axios.post(api_url + '/api/grooming-service-bookings', booking_data)
             .then((res) => {
                 console.log(res);
                 if (res.data.isSucceed === true) {
@@ -63,7 +79,7 @@ export default function BookServiceDialog(props) {
                         <Typography variant="h4" color="blue-gray">
                             Đặt dịch vụ
                         </Typography>
-                        <Input required name="date" value={booking_data.bookingDate} onChange={handleChange} label="Ngày" type="date" size="md" />
+                        <Input required name="bookingDate" value={booking_data.bookingDate} onChange={handleChange} label="Ngày" type="date" size="md" />
                         <Input className="w-1/2" name="price" value={booking_data.notes} onChange={handleChange} label="Ghi chú" type="text" size="md" placeholder="Thêm ghi chú hoặc để trống" />
                     </CardBody>
                     <CardFooter className="pt-0">
