8129b48f25cd602346d989b8bc5f018a4d689d69
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2024-06-10 15:03:38 +0700

    Commit

--- PetCareSystem/PetCareSystem/obj/Release/net8.0/PetCareSystem.AssemblyInfo.cs
+++ PetCareSystem/PetCareSystem/obj/Release/net8.0/PetCareSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetCareSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75a052f8f1978badd1b71cb5a17b8455c9fc2cdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de811c1125369d56370ea40f8ef3e09da04d21cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetCareSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetCareSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
