28b3f5ab601b883f782a245151221809b591d867
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2024-06-10 15:38:06 +0700

    Merge

--- PetCareSystem/PetCareSystem.Test/obj/Release/net8.0/PetCareSystem.Test.AssemblyInfo.cs
+++ PetCareSystem/PetCareSystem.Test/obj/Release/net8.0/PetCareSystem.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetCareSystem.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c64b1fda2be50105b3da61e24d5ea7a67743107f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52d887f99503ae4e13ff366f0fd0761656cc207d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetCareSystem.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetCareSystem.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
